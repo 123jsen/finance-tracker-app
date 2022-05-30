2ddc2d0fc8adc892c2537a518a508fdfd73eed1c
SenYanHo <80093853+123jsen@users.noreply.github.com>
2022-05-30 14:24:03 +0800

    Configure tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// https://tailwindcss.com/docs/installation/using-postcss
+
+module.exports = {
+  content: ["./client/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
